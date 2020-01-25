0a469c6d9b167cf966985ca9fd81b62b8c7eb6ab
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2020-01-24 21:32:52 -0300

    finalização do cartão

--- bin/card.js
+++ bin/card.js
@@ -1,28 +1,34 @@
+#!/usr/bin/env node
+
 const chalk = require("chalk");
 const boxen = require("boxen");
 
-const nome = chalk.white("João da Silva");
-const apelido = chalk.white("joaosilva");
+const nome = chalk.white("Tiago Lage Payne de Pádua");
+const apelido = chalk.white("tiagolpadua");
 
 const rotOcupacao = chalk.white.bold("Ocupação:");
-const ocupacao = chalk.white("Analista Junior na IdeiaSoft");
+const ocupacao = chalk.white("Analista Sênior no Banco do Brasil");
 
 const rotTwitter = chalk.white.bold("Twitter:".padStart(9));
-const twitter = chalk.grey("https://twitter.com/") + chalk.cyan("joaosilva");
+const twitter = chalk.grey("https://twitter.com/") + chalk.cyan("tiagolpadua");
 
-const rotGithub = chalk.white.bold("GitHub:".padStart(9));
-const github = chalk.grey("https://github.com/") + chalk.green("joaosilva");
+const rotGithub = chalk.white.bold("GMail:".padStart(9));
+const github = chalk.yellow("tiagolpadua@gmail.com");
+
+const rotGmail = chalk.white.bold("GitHub:".padStart(9));
+const gmail = chalk.grey("https://github.com/") + chalk.green("tiagolpadua");
 
 const rotLinkedIn = chalk.white.bold("LinkedIn:");
 const linkedIn =
-  chalk.grey("https://linkedin.com/in/") + chalk.blue("joaosilva");
+  chalk.grey("https://linkedin.com/in/") + chalk.blue("tiagolpadua");
 
 const rotCartao = chalk.white.bold("Cartão:".padStart(9));
-const cartao = chalk.red("npx") + " " + chalk.white("joaosilva");
+const cartao = chalk.red("npx") + " " + chalk.white("tiagolpadua");
 
-let saida = chalk.green(`         ${nome} / ${apelido}\n\n`);
+let saida = chalk.green(`   ${nome} / ${apelido}\n\n`);
 saida += `${rotOcupacao} ${ocupacao}\n\n`;
 saida += `${rotTwitter} ${twitter}\n`;
+saida += `${rotGmail} ${gmail}\n`;
 saida += `${rotGithub} ${github}\n`;
 saida += `${rotLinkedIn} ${linkedIn}\n\n`;
 saida += `${rotCartao} ${cartao}`;
